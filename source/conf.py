5a57c1762d3c2ce7752fc09fbfcd3c4ac7087a0f
Jud Dagnall <github@dagnall.net>
2024-10-06 02:00:58 -0700

    Add github links

--- source/conf.py
+++ source/conf.py
@@ -10,6 +10,8 @@ project = 'The Text Processing Cookbook'
 copyright = '2024, Jud Dagnall'
 author = 'Jud Dagnall'
 release = '0.1'
+github_user = 'thejud'
+github_repo = 'the-text-processing-cookbook'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -37,6 +39,8 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 html_theme_options = {
         'show_relbars': True,
+        'github_banner' : True,
+        'github_button' : True,
 }
 html_logo = '80s_keycaps.png'
 smart_quotes = False
@@ -45,4 +49,5 @@ myst_heading_anchors = 3
 myst_enable_extensions = [
     'colon_fence',
 ]
+
 
